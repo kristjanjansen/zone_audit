bc04a343c348efe8ff0765123dc94977e0d60d78
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-31 18:31:49 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
-var port = 6000
+var port = 5000
 
 app.use(express.static('public'));
 server.listen(port);
