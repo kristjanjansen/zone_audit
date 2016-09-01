3ecdc3a3312f408bd86b9c032199b9641181f148
kristjanjansen <kristjan.jansen@gmail.com>
2016-09-01 10:41:48 +0300

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var elixir = require('laravel-elixir')
 
 require('laravel-elixir-vue')
+require('./svg');
 
 elixir(function(mix) {
     mix.webpack('./src/main.js', './public/js')
+    mix.svg('./src/svg', './public/svg');
 })
---
+++ src/components/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <svg style="width: 1.5em; height: 1.5em">
+          <use :xlink:href="'./svg/svg.svg#' + icon"/>
+    </svg>
+
+</template>
+
+<script>
+
+    export default {
+        props: {
+            icon: {required: true}
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
